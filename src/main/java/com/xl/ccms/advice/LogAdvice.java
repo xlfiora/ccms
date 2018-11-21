96bacfa4ceb4fa94e8ad480d3275ee928446e544
xl <297913871@qq.com>
2018-11-21 21:15:13 +0800

    ccms项目环境搭建

---
+++ src/main/java/com/xl/ccms/advice/LogAdvice.java
@@ -0,0 +1,7 @@
+package com.xl.ccms.advice;
+
+/**
+ * Created by xl on 2018/11/18 0018.
+ */
+public class LogAdvice {
+}
---
+++ src/main/java/com/xl/ccms/config/MyRealm.java
@@ -0,0 +1,23 @@
+package com.xl.ccms.config;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by xl on 2018/11/18 0018.
+ */
+public class MyRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
