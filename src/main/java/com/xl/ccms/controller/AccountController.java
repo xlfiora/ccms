8518f59b9dbce6fdd3db9e61170a1c52d41bf2b6
wentao <wentao.z@sunnyintec.com>
2019-01-09 18:08:47 +0800

    学生用户管理设计2.0

---
+++ src/main/java/com/xl/ccms/controller/AccountController.java
@@ -0,0 +1,42 @@
+package com.xl.ccms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2019/1/9.
+ */
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @RequestMapping("/login")
+    public void login(){
+
+    }
+
+    @RequestMapping("/addStu")
+    public void addStu(){
+
+    }
+
+    @RequestMapping("/removeStu")
+    public void removeStu(){
+
+    }
+
+    @RequestMapping("/modifyStu")
+    public void modifyStu(){
+
+    }
+
+    @RequestMapping("/searchAllStu")
+    public void searchAllStu(){
+
+    }
+
+
+
+
+
+}
